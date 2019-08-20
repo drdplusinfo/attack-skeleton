2c8f8cd3f8234f4c6a69e79280b0f675d82674e9
Jaroslav Týc <mail@jaroslavtyc.com>
2019-08-20 14:48:56 +0200

    A test runs only on skeleton

--- DrdPlus/Tests/AttackSkeleton/TestsConfigurationTest.php
+++ DrdPlus/Tests/AttackSkeleton/TestsConfigurationTest.php
@@ -8,6 +8,36 @@ class TestsConfigurationTest extends \DrdPlus\Tests\CalculatorSkeleton\TestsConf
 {
     use Partials\AttackCalculatorTestTrait;
 
+    /**
+     * @test
+     * @dataProvider provideStrictBooleanConfiguration
+     * @param string $directive
+     * @param $value
+     */
+    public function Skeleton_boolean_tests_configuration_is_strict(string $directive, $value)
+    {
+        if (!$this->isSkeletonChecked()) {
+            self::assertFalse(false, 'It is hard to test on real calculators');
+            return;
+        }
+        parent::Skeleton_boolean_tests_configuration_is_strict($directive, $value);
+    }
+
+    /**
+     * @test
+     * @dataProvider provideStrictArrayConfiguration
+     * @param string $directive
+     * @param bool $hasContent
+     */
+    public function Skeleton_array_tests_configuration_is_strict(string $directive, bool $hasContent)
+    {
+        if (!$this->isSkeletonChecked()) {
+            self::assertFalse(false, 'It is hard to test on real calculators');
+            return;
+        }
+        parent::Skeleton_array_tests_configuration_is_strict($directive, $hasContent);
+    }
+
     protected static function getSutClass(string $sutTestClass = null, string $regexp = '~(.+)Test$~'): string
     {
         return TestsConfiguration::class;
